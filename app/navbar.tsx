b0ac4d689b819a233f2a09cd8eb24aa96999d49b
chamalka Jayawardhana <chamalka8mini@gmail.com>
2025-07-21 08:54:18 +0530

    Style active link in navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,17 +1,40 @@
+'use client';
+
 import React from 'react'
 import Link from 'next/link'
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation';
 
-const navbar = () => {
+const Navbar = () => {
+  const currentPath = usePathname();
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'><FaBug/></Link>
-        <ul className='flex space-x-6'>
-            <li><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href=''>Dashboard</Link></li>
-            <li><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href='/users'>Users</Link></li>
-        </ul>
+      <Link href='/'><FaBug /></Link>
+      <ul className='flex space-x-6'>
+        <li>
+          <Link
+            href='/'
+            className={`${
+              currentPath === '/' ? 'text-zinc-900' : 'text-zinc-500'
+            } hover:text-zinc-800 transition-colors`}
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='/users'
+            className={`${
+              currentPath === '/users' ? 'text-zinc-900' : 'text-zinc-500'
+            } hover:text-zinc-800 transition-colors`}
+          >
+            Users
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
